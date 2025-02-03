df8b2ed24c5a9d587341aa274aece6187d652877
Yuna_H <ksm13563661@gmail.com>
2025-02-04 05:05:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -7,9 +7,9 @@ int r1, r2, list[DWARF];
 void solve() {
         //중첩 반복문으로 제외할 두 값 결정
         //전체 합에서 두 값을 뺐을 때 100이 되는 경우 찾기
+        int sum = accumulate(list, list + DWARF, 0);
         for (int i = 0; i < DWARF; i++) {
                 for (int j = 0; j < DWARF; j++) {
-                        int sum = accumulate(list, list + DWARF, 0);
                         if (sum - list[i] - list[j] == 100) {
                                 r1 = i; r2 = j;
                                 return;
