b6ca5d6af95074e2063092ed9ec83002a240af14
Yuna_H <ksm13563661@gmail.com>
2025-02-14 21:23:54 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,43 +1,19 @@
-#include <iostream>
+#include<iostream>
+#include<algorithm>
+#define MAX_SIZE 20
 using namespace std;
-
-#define MAX 16
-
 int N;
-int Ti[MAX]={0,};
-int Pi[MAX]={0,};
-int res[MAX]={0,};
-
-int Max(int a, int b){
-	return a>b ? a : b;
-}
-
-void Input(){
-	cin >> N;
-	for (int i=1; i<=N; i++){
-		cin >> Ti[i] >> Pi[i];
-	}
-}
-
-void Dp(){
-	int deadline;
-	for (int i=N; i>0; i--){
-		deadline = i + Ti[i];
-		if (deadline > N+1){
-			// 상담 불가
-			res[i] = res[i+1];
-		}
-		else {
-			// 상담 가능, 최대 이익 판별 필요
-			res[i] = Max(res[i+1], res[deadline] + Pi[i]);
-		}
-	}
-}
-
-int main() {
-	Input();
-	Dp();
-	
-	cout << res[1] << endl;
-	return 0;
+int ans[MAX_SIZE] = { 0, }; //ans[i] = i번째 날부터 얻을 수 있는 최대 수익
+int Ti[MAX_SIZE], Pi[MAX_SIZE];
+int main()
+{
+        cin >> N;
+        for (int i = 1; i <= N; i++) {
+                cin >> Ti[i] >> Pi[i];
+        }
+        for (int i = N; i > 0; i--) {
+                if(i+Ti[i]>N+1) ans[i] = ans[i + 1];
+                else ans[i] = max(ans[i + 1], ans[i+Ti[i]]+Pi[i]);
+        }
+        cout << ans[1] << endl;
 }
