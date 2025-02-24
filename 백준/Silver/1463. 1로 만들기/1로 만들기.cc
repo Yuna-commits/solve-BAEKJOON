d97976405bb03633538b6ca9834b8373fe347741
Yuna_H <ksm13563661@gmail.com>
2025-02-25 03:22:09 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,30 +1,22 @@
-/*
-N을 1로 만들기 위한 최소 횟수
-=((N-1)을 1로 만들기 위한 최소 횟수) + 1
-2~N은 모두 1이 되기 위한 과정으로 동일한 연산을 취함
-=>즉 N이 연산으로 정수 K가 될 때, 
-K가 1이 되기 위한 연산 횟수가 X번이라면 N이 1이 되기 위한 연산 횟수는 (X + 1)번
-*/
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#define min(a,b)(a<b?a:b)
-#define MAX_SIZE 10000001
-int dp[MAX_SIZE];
-int N;
-int solve() {
-        /*dp[N]=min(dp[N-1]+1, dp[N/X]+1)
-        => ((N-1)을 1로 만들기 위한 최소 횟수) + 1와 ((N/X)를 1로 만들기 위한 최소 횟수) + 1 비교*/
-        for (int i = 2; i <= N; i++) {
-                dp[i] = dp[i - 1] + 1;
-                if (i % 2 == 0)
-                        dp[i] = min(dp[i], dp[i / 2] + 1);
-                if (i % 3 == 0)
-                        dp[i] = min(dp[i], dp[i / 3] + 1);
-        }
-        return dp[N];
-}
+#include<iostream>
+#include<utility>
+#define N_SIZE 1000001
+#define INF 987654321
 int main()
 {
-        scanf("%d", &N);
-        printf("%d\n", solve());
+        using namespace std;
+        int N; cin >> N;
+        int dp[N_SIZE];//N -> 1, dp[i] = N으로 i를 만들기 위한 연산 횟수
+        fill(dp, dp+N_SIZE, INF);//0번째부터 (N+1)개를 INF로 초기화
+        dp[N] = 0;
+        for (int i = N; i > 1; i--) {
+                if (i % 3 == 0) {
+                        dp[i / 3] = min(dp[i / 3], dp[i] + 1);
+                }
+                if (i % 2 == 0) {
+                        dp[i / 2] = min(dp[i / 2], dp[i] + 1);
+                }
+                dp[i - 1] = min(dp[i - 1], dp[i] + 1);
+        }
+        cout << dp[1];
 }
