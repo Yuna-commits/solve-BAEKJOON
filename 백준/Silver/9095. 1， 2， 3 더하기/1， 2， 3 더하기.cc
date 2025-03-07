17488c94342f390f906a355ee277624703bd33e3
Yuna_H <ksm13563661@gmail.com>
2025-03-07 22:44:12 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,18 +1,16 @@
 #include<iostream>
 #define N_SIZE 11
 using namespace std;
-int T;
-int dp[N_SIZE];
 //dp[i] == i를 1, 2, 3의 합으로 나타내는 방법의 수
 int main() {
-        fill(&dp[0], &dp[N_SIZE], 0);
+        int T;
+        int dp[N_SIZE] = { 0, };
         dp[1] = 1; dp[2] = 2; dp[3] = 4;
+        for (int i = 4; i < N_SIZE; i++)
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         cin >> T;
         while (T--) {
                 int n; cin >> n;
-                for (int i = 4; i <= n; i++) {
-                        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-                }
                 cout << dp[n] << endl;
         }
 }
