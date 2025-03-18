daa36e8a954d79188cc766115050179e96c91702
Yuna_H <ksm13563661@gmail.com>
2025-03-19 01:31:37 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.cc
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.cc
@@ -1,34 +1,20 @@
 #include<iostream>
-#include<queue>
-#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
-#define MAX_SIZE 1000001
+#include<algorithm>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define N_SIZE 1000001
 using namespace std;
-int graph[MAX_SIZE];
-void BFS(int start, int end) {
-        queue<int> que;
-        que.push(start);
-        graph[start] = 1;
-        while (graph[end] == 0) {
-                int now = que.front();
-                que.pop();
-                if (now + 1 <= 1000000) {
-                        if (graph[now + 1] == 0 || graph[now + 1] > graph[now] + 1) {
-                                que.push(now + 1);
-                                graph[now + 1] = graph[now] + 1;
-                        }
-                }
-                if (now * 2 <= 1000000) {
-                        if (graph[now * 2] == 0 || graph[now * 2] > graph[now] + 1) {
-                                que.push(now * 2);
-                                graph[now * 2] = graph[now] + 1;
-                        }
-                }
-        }
-}
+int A, K;
+int dp[N_SIZE];//dp[i] == A가 i를 만드는 최소 연산 횟수
 int main() {
         fastio;
-        int A, K;
         cin >> A >> K;
-        BFS(A, K);
-        cout << graph[K] - 1;
+        fill(&dp[A], &dp[K + 1], 987654321);
+        dp[A] = 0;
+        for (int i = A + 1; i <= K; i++) {
+                if (i % 2 == 0 && i / 2 >= A) {//i가 짝수면 연산1, 연산2 비교
+                        dp[i] = min(dp[i] + 1, dp[i / 2] + 1);
+                }
+                else  dp[i] = dp[i - 1] + 1;
+        }
+        cout << dp[K];
 }
